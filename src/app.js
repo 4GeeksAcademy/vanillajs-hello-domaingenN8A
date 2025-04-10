e842bbb54b05875143eb281782444e4351879517
Natalie8a <naty.8a@outlook.com>
2025-04-10 23:16:13 +0000

    finaldomaingN8A

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let tld = ['.com','.net','.gov']
+  
   console.log("Hello Rigo from the console!");
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < tld.length; l++) {
+          console.log(pronoun[i]+adj[j]+noun[k]+tld[l])
+          
+        }
+       
+        
+      }
+      
+    }
+  }
 };
